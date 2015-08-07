6b77e88f661c582f793f20869da40637a4492d2a
Pavel Nezhencev <pavel.nezhencev@creuna.com>
2015-08-07 16:24:19 +0300

    Update assembly infos

--- RestImageResize.EpiServer/Properties/AssemblyInfo.cs
+++ RestImageResize.EpiServer/Properties/AssemblyInfo.cs
@@ -1,36 +1,12 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
+// General info about the product
 [assembly: AssemblyTitle("RestImageResize.EPiServer")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("ciklum")]
-[assembly: AssemblyProduct("RestImageResize.EPiServer")]
-[assembly: AssemblyCopyright("Copyright © ciklum 2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyProduct("RestImageResize")]
+[assembly: AssemblyDescription("Includes required dependences and source code for easily start to use RestImageResize package in EPiServer CMS site.")]
+[assembly: AssemblyCompany("Roman Mironets, Creuna Kharkiv office")]
+[assembly: AssemblyCopyright("Copyright © Roman Mironets 2015")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d0a87415-cda8-4f48-a1ee-395a1c0a1348")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// Product version
+[assembly: AssemblyVersion("1.1")]
+[assembly: AssemblyFileVersion("1.1")]
--- RestImageResize/Properties/AssemblyInfo.cs
+++ RestImageResize/Properties/AssemblyInfo.cs
@@ -1,35 +1,11 @@
 ﻿using System.Reflection;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
+// General info about the product
 [assembly: AssemblyTitle("RestImageResize")]
-[assembly: AssemblyProduct("RestImageResize")]
 [assembly: AssemblyDescription("Provides HttpModule that adds support of simple rest API to OpenWaves.ImageTransformations.Web package.")]
 [assembly: AssemblyCompany("Roman Mironets, Creuna Kharkiv office")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCopyright("Copyright © Roman Mironets 2015")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e95290d8-60e3-46bf-9b66-de20523fea83")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-
+// Product version
 [assembly: AssemblyVersion("1.0.3")]
 [assembly: AssemblyFileVersion("1.0.3")]
