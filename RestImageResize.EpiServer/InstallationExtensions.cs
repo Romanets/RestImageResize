1e8af54dd4990fb0622401563da824567c64b7d9
Pavel Nezhencev <pavel.nezhencev@creuna.com>
2015-08-07 16:16:34 +0300

    Add installation code to episerver project

---
+++ RestImageResize.EpiServer/InstallationExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Hosting;
+using EPiServer.ImageLibrary;
+using OpenWaves;
+using OpenWaves.ImageTransformations.EPiServer;
+using OpenWaves.ImageTransformations.Web;
+
+namespace RestImageResize.EPiServer
+{
+    public static class InstallationExtensions
+    {
+        public static BasicResolver RegisterRestImageResize(this BasicResolver resolver)
+        {
+            var imageDataFileProvider = global::EPiServer.ServiceLocation.ServiceLocator.Current.GetInstance<ImageDataFileProvider>();
+            var webImageTransformService = new WebImageTransformationService(
+                new VirtualFileProviderCombiner(imageDataFileProvider, new EPiVirtualPathFileProvider(HostingEnvironment.VirtualPathProvider)),
+                new ConcurrentFileStore(new VirtualFileStore("ImagesTransformVPP")),
+                new EPiImageTransformationService(new ImageService()));
+
+            var validationRules = new IImageTransformationUrlValidationRule[]
+            {
+                new ForEditorsImageTransformationUrlValidationRule(),
+                new HmacImageTransformationUrlValidationRule()
+            };
+
+            var moduleImplementation = new WebImageTransformationModuleImplementation(webImageTransformService, validationRules);
+
+            resolver
+                .Register<IWebImageTransformationService>(moduleImplementation)
+                .Register<IWebImageTransformationModuleImplementation>(moduleImplementation);
+
+            return resolver;
+        }
+    }
+}
