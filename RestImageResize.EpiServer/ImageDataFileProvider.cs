fc1b18647778226c349c3395bcea753cab6cf2ec
Pavel Nezhencev <pavel.nezhencev@creuna.com>
2015-08-07 16:04:43 +0300

    Create new RestImageResize.EPiServer project with fixes for EPiServer 7.5+

---
+++ RestImageResize.EpiServer/ImageDataFileProvider.cs
@@ -0,0 +1,31 @@
+﻿using EPiServer.Core;
+using EPiServer.Web.Routing;
+using OpenWaves;
+using OpenWaves.ImageTransformations.Web;
+
+namespace RestImageResize.EPiServer
+{
+    public class ImageDataFileProvider : IVirtualFileProvider
+    {
+        public ImageDataFileProvider(UrlResolver urlResolver)
+        {
+            UrlResolver = urlResolver;
+        }
+
+        protected virtual UrlResolver UrlResolver { get; private set; }
+
+        public IVirtualFile GetFile(Url fileUrl)
+        {
+            var contentRef = UrlResolver.Route(new global::EPiServer.UrlBuilder(fileUrl));
+
+            var image = contentRef as ImageData;
+
+            if (image != null)
+            {
+                return new ImageDataVirtualFile(fileUrl, image);
+            }
+
+            return null;
+        }
+    }
+}
---
+++ RestImageResize.EpiServer/ImageDataVirtualFile.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+using System.IO;
+using EPiServer.Core;
+using OpenWaves;
+using OpenWaves.ImageTransformations.Web;
+
+namespace RestImageResize.EPiServer
+{
+    public class ImageDataVirtualFile : IVirtualFile
+    {
+        public ImageDataVirtualFile(Url url, ImageData image)
+        {
+            Image = image;
+            Url = url;
+        }
+
+        public virtual Stream Open()
+        {
+            
+            return Image.BinaryData.OpenRead();
+        }
+
+        public virtual Url Url { get; private set; }
+
+        public string Hash { get { return Image.Changed.ToFileTime().ToString(CultureInfo.InvariantCulture); } }
+
+        protected ImageData Image { get; private set; }
+    }
+}
